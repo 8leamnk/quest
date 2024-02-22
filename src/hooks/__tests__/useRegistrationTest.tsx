5ef1192f43a26228e2a6c2cb5a7d4d968b170210
8leamnk <silkviewrk@gmail.com>
2024-02-22 15:24:11 +0900

    test: 퀘스트 등록 기능 테스트

---
+++ src/hooks/__tests__/useRegistrationTest.tsx
@@ -0,0 +1,28 @@
+import { act, renderHook } from '@testing-library/react';
+import { Quest } from '../../constants/types';
+import useRegistration from '../useRegistration';
+
+describe('퀘스트 등록 기능 테스트', () => {
+  test('사용자가 등록한 순서대로 퀘스트가 등록된다.', () => {
+    // given
+    const INPUT_AND_OUTPUT: Quest[] = [
+      { title: '제목', content: '내용', priority: 3 },
+      { title: '제목', content: '내용', priority: 1 },
+      { title: '제목', content: '내용', priority: 4 },
+      { title: '제목', content: '내용', priority: 7 },
+      { title: '제목', content: '내용', priority: 2 },
+    ];
+
+    // when
+    const { result, rerender } = renderHook(useRegistration);
+    INPUT_AND_OUTPUT.forEach((input) => {
+      act(() => {
+        result.current.registerQuest(input);
+        rerender();
+      });
+    });
+
+    // then
+    expect(result.current.registeredData).toEqual(INPUT_AND_OUTPUT);
+  });
+});
