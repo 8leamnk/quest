698add428ac44043d2fdf9b99c22f8ffb8fa2301
8leamnk <silkviewrk@gmail.com>
2024-02-22 14:41:31 +0900

    feat: 퀘스트 입력 유효성 검사 기능 추가

--- src/hooks/useValidation.tsx
+++ src/hooks/useValidation.tsx
@@ -2,10 +2,19 @@ import { useCallback, useState } from 'react';
 import { QuestInputs } from '../constants/types';
 
 const MIN_PRIORITY: number = 1;
+const ERROR_EMPTY: string = '입력창 전부 입력해 주세요.';
 const ERROR_PRIORITY_RANGE: string = '중요도는 1 이상의 숫자를 입력해 주세요.';
 
 function useValidation() {
   const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const validateEmpty = useCallback((inputs: QuestInputs) => {
+    Object.values(inputs).forEach((value) => {
+      if (value.length === 0) {
+        throw new Error(ERROR_EMPTY);
+      }
+    });
+  }, []);
 
   const validateRange = useCallback((priority: string) => {
     if (Number(priority) < MIN_PRIORITY) {
@@ -16,6 +25,7 @@ function useValidation() {
   const validate = useCallback(
     (inputs: QuestInputs) => {
       try {
+        validateEmpty(inputs);
         validateRange(inputs.priority);
         setErrorMessage('');
         return true;
