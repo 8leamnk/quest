627b28ebe46463e3dbfe34689964d7f9e6e64e95
8leamnk <silkviewrk@gmail.com>
2024-03-01 22:08:24 +0900

    refactor: 메인 컨트롤러 리팩토링
    변경 내용:
    - 페이지 경로 변경된 것 반영
    - 공식 문서에 따라 path에 '/' 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './routes/Home';
-import QuestStart from './routes/QuestStart';
-import NotFound from './routes/NotFound';
+import Home from './Pages/Home';
+import QuestStart from './Pages/QuestStart';
+import NotFound from './Pages/NotFound';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <Home />, errorElement: <NotFound /> },
-    { path: '/quest-start', element: <QuestStart /> },
+    { path: 'quest-start', element: <QuestStart /> },
   ]);
 
   return <RouterProvider router={router} />;
