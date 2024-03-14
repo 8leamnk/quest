5f2753ef7d9dc8dce5dfc57c11d2079d4975a4da
8leamnk <silkviewrk@gmail.com>
2024-03-14 21:33:40 +0900

    feat: 404 페이지 기능 추가

--- src/Components/Pages/NotFound.tsx
+++ src/Components/Pages/NotFound.tsx
@@ -1,5 +1,14 @@
+import MainSectionLayout from '../Layout/MainSectionLayout';
+import Notice from '../Molecules/Notice';
+
 function NotFound() {
-  return <>404 not found</>;
+  return (
+    <MainSectionLayout>
+      <Notice pathname="/" linkText="메인 페이지로">
+        없는 페이지입니다.
+      </Notice>
+    </MainSectionLayout>
+  );
 }
 
 export default NotFound;
